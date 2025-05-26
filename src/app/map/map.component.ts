6e34634603f420114a405bb7fe4e1d167d8f0c24
Marius Winterscheid <44503013+MariusWinterscheid@users.noreply.github.com>
2025-05-26 17:02:40 +0200

    add osm and map component

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,27 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements AfterViewInit {
+  private map!: L.Map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [51.019, 7.243], // default coordinates (Kürten (Biesfeld), Germany)
+      zoom: 15,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+}
